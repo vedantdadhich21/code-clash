327d6bca4d2d421448d3bb93b1af7050b227e05f
Vedant Dadhich <vedantdadhich21@gmail.com>
2026-07-01 18:42:27 +0530

    fix: force authDomain to use window hostname

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,7 +4,7 @@ import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  authDomain: window.location.hostname === 'localhost' ? import.meta.env.VITE_FIREBASE_AUTH_DOMAIN : window.location.hostname,
   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
